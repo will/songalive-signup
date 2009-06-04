2d491f82c647778a5d6e0e1174b110cb02e49ee6
Will Leinweber <will@bitfission.com>
2009-06-04 15:02:36 -0500

    what

--- email_collector.rb
+++ email_collector.rb
@@ -2,6 +2,20 @@ require 'rubygems'
 require 'sinatra'
 require 'haml'
 
+require 'signup.rb'
+require 'email_regex'
+
 get '/' do
   haml :index
+end
+
+post '/' do
+  email = params[:email]
+  
+  if email =~ EmailAddress
+    "good email"
+  else
+    "bad email"
+  end
+  
 end
