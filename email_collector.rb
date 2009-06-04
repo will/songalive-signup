9e20032c247b30fb41c5e9bb287ddeecb7bc32be
Will Leinweber <will@bitfission.com>
2009-06-04 15:38:35 -0500

    all functionality

--- email_collector.rb
+++ email_collector.rb
@@ -3,19 +3,19 @@ require 'sinatra'
 require 'haml'
 
 require 'signup.rb'
-require 'email_regex'
+
 
 get '/' do
   haml :index
 end
 
 post '/' do
-  email = params[:email]
+  Signup.signup(params[:email])
   
-  if email =~ EmailAddress
-    "good email"
-  else
-    "bad email"
-  end
-  
+  redirect '/thanks'
+end
+
+get '/thanks' do
+  @thanks = true
+  haml :index
 end
--- signup.rb
+++ signup.rb
@@ -1,6 +1,8 @@
+require 'email_regex'
 require "dm-core"
 require "dm-timestamps"
- DataMapper.setup(:default, "sqlite3:///#{Dir.pwd}/emails.db")
+
+DataMapper.setup(:default, "sqlite3:///#{Dir.pwd}/emails.db")
 
 class Signup
   include DataMapper::Resource
@@ -8,4 +10,10 @@ class Signup
   property :id, Integer, :serial => true
   property :email, String, :key => true
   timestamps :at
+  
+  def self.signup(email)
+    self.create(:email => email) if email =~ EmailAddress
+  end
 end
+
+Signup.auto_upgrade!
